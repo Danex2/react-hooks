8bd3e7e1356d986c37466f02004cb573607bc29f
Danex2 <khadane.miller@gmail.com>
2019-04-11 18:25:17 -0400

    counts down instead of up

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,17 +2,17 @@ import React, { useState, useEffect } from "react";
 
 function Counter() {
   const [count, increment] = useState(0);
-  const [timer, countdown] = useState(0);
+  const [timer, countdown] = useState(30);
   const [disabled, setDisabled] = useState(false);
 
   useEffect(() => {
-    if (timer <= 29) {
+    if (timer > 0) {
       var inter = setInterval(function() {
         // only works properly if i count up instead of down?
-        countdown(timer => timer + 1);
+        countdown(timer => timer - 1);
       }, 1000);
     }
-    if (timer === 30) {
+    if (timer === 0) {
       setDisabled(true);
     }
     return () => clearInterval(inter);
@@ -23,7 +23,7 @@ function Counter() {
       <h1>Timer: {timer}</h1>
       <h3>{`The current count is ${count}`}</h3>
       <button disabled={disabled} onClick={() => increment(count => count + 1)}>
-        Increment
+        Click!
       </button>
       <h4>How many clicks can you get in 30 seconds?</h4>
     </div>
